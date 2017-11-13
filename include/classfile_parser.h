8e89a700b03dd39d4df090eb0e43fa93738ffd57
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-11-13 06:58:45 +0100

    Base files

---
+++ include/classfile_parser.h
@@ -0,0 +1,13 @@
+#ifndef CLASSFILE_PARSER_H
+#define CLASSFILE_PARSER_H
+
+#include <classfile/export.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CLASSFILE_PARSER_H */
---
+++ include/classfile_writer.h
@@ -0,0 +1,13 @@
+#ifndef CLASSFILE_WRITER_H
+#define CLASSFILE_WRITER_H
+
+#include <classfile/export.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CLASSFILE_WRITER_H */
